dc8461e571055071df36d71e932ba3a8e8322ac1
amokrynskii <amokrynskii@team.amocrm.com>
2022-09-10 20:17:35 +0300

    Update migrate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-
+        <title>Test App</title>
     </head>
     <body class="antialiased">
         <h1>Hello</h1>
