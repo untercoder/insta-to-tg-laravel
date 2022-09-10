8f520783883d51d6cf68b4508d0e9077a2869bb9
amokrynskii <amokrynskii@team.amocrm.com>
2022-09-10 19:50:00 +0300

    My rout for test db

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db', function (\http\Env\Request $request) {
+    dd($request);
+    return "Im work";
+});
